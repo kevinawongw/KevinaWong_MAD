bd59548381900fec04607a03d8e81a8347425134
Kevina Wong <kevinawongw@gmail.com>
2021-10-19 10:50:07 -0600

    updates

--- ClassApp6/app/src/main/java/com/example/halloweenapp/MainActivity.kt
+++ ClassApp6/app/src/main/java/com/example/halloweenapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.halloweenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     fun sayBoo(view: android.view.View) {
         val booText = findViewById<TextView>(R.id.textMessage)
-        booText.text = "Happy Halloweeeennn"
+        val nameText = findViewById<EditText>(R.id.editTextName)
+        val imageGhost = findViewById<ImageView>(R.id.imageView)
+
+        booText.text = "Happy Halloweeeennn "  + nameText.text + "!"
+        imageGhost.setImageResource(R.drawable.ghost)
+
+
     }
 
 
